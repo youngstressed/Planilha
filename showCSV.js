009e8703781a76bc5ddd12ed28c5da11fdd771aa
Gabriel <gabriel.sansao022@gmail.com>
2024-09-03 14:57:30 -0300

    Atualização do CSV

--- showCSV.js
+++ showCSV.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const csv = fs.readFileSync('./oscar_best_pictures.csv').toString();
 const {
     quebraLinhas,separaColunas,separaConteudo,parteDificil
-} = require('./lib/csv');
+} = require('csv');
 
 const linhas = quebraLinhas(csv);
 const primeiraLinha = linhas.shift();
