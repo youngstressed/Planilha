22895e9a8d345173b4a416a28cf66ab1ac98f0d5
Gabriel <gabriel.sansao022@gmail.com>
2024-09-03 14:38:37 -0300

    Importar funções

--- showCSV.js
+++ showCSV.js
@@ -1,7 +1,9 @@
 //// Função Principal
 const fs = require('fs');
 const csv = fs.readFileSync('./oscar_best_pictures.csv').toString();
-
+const {
+    quebraLinhas,separaColunas,separaConteudo,parteDificil
+} = require('./csv');
 const linhas = quebraLinhas(csv);
 const primeiraLinha = linhas.shift();
 const headers = separaColunas(primeiraLinha);
@@ -14,53 +16,3 @@ console.table(json, ["Filme", "Ano", "Gênero"]);
 
 /////////////////////////////
 
-function quebraLinhas(csv) {
-    let linhas = csv.trim().split('\n');
-    return linhas;
-}
-
-function separaColunas(linha) {
-    let colunas = linha.trim().split(';')
-    return colunas;
-}
-
-function separaConteudo(lines) {
-    const conteudo = [];
-
-    for(let i=0; i<lines.length; i++) {
-        let linha = lines[i];
-        console.log(linha);
-        let colunas = separaColunas(linha);
-        console.log(colunas);
-
-        conteudo.push(colunas);
-    
-    }
-
-    return conteudo;
-}
-
-function constroiObjeto(headers,linhaTabela) {
-    let obj = {};
-    for(let j=0; j<headers.length; j++){
-        let titulo = headers[j];
-        let coluna = linhaTabela[j];
-
-        obj[titulo] = coluna;
-    }
-        return obj;
-}
-
-
-function parteDificil(headers, conteudo) {
-    const result = [];
-
-    for(let i=0; i<conteudo.length; i++) {
-       const linhaTabela = conteudo[i];
-       const obj = constroiObjeto(headers,linhaTabela)
-    
-        result.push(obj);
-    }
-
-    return result;
-}
